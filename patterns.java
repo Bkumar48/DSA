932a4252fc0dff31269e91bfbdf1ea0ec245ce93
Bkumar48 <bittukumarinfo48@gmail.com>
2026-05-07 10:26:39 +0530

    Add diamond pattern method and update main method to demonstrate it

--- patterns.java
+++ patterns.java
@@ -122,6 +122,27 @@ public class patterns {
         }
     }
 
+    public static void diamond(int n) {
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n - i; j++) {
+                System.out.print(" ");
+            }
+            for (int j = 1; j <= 2 * i - 1; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        for (int i = n - 1; i >= 1; i--) {
+            for (int j = 1; j <= n - i; j++) {
+                System.out.print(" ");
+            }
+            for (int j = 1; j <= 2 * i - 1; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         // System.out.println("Enter the rows and columns of the rectangle: ");
@@ -134,8 +155,9 @@ public class patterns {
         // floydsTriangle(5);
         // zeroOneTriangle(5);
         // butterfly(4);
-        rhombus(5);
-        hollowRhombus(5);
+        // rhombus(5);
+        // hollowRhombus(5);
+        diamond(5);
         sc.close();
     }
 }
