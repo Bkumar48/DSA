918146f8d56c5c3b9ca75e9662d1e59ee38913ca
Bkumar48 <bittukumarinfo48@gmail.com>
2026-05-07 23:27:45 +0530

    Implement number pyramid patterns: numberPyramid and palindromic number diamond

--- patterns.java
+++ patterns.java
@@ -143,6 +143,33 @@ public class patterns {
         }
     }
 
+    public static void numberPyramid(int n) {
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n - i; j++) {
+                System.out.print("-");
+            }
+            for (int j = 1; j <= i; j++) {
+                System.out.print(i + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void p(int n) {
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n - i; j++) {
+                System.out.print(" ");
+            }
+            for (int j = i; j >= 1; j--) {
+                System.out.print(j);
+            }
+            for (int j = 2; j <= i; j++) {
+                System.out.print(j);
+            }
+            System.out.println();
+        }
+    }
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         // System.out.println("Enter the rows and columns of the rectangle: ");
@@ -157,7 +184,9 @@ public class patterns {
         // butterfly(4);
         // rhombus(5);
         // hollowRhombus(5);
-        diamond(5);
+        // diamond(5);
+        // numberPyramid(5);
+        p(5);
         sc.close();
     }
 }
