5de1bc2432353621f0ede796b326e3e1beddfa19
Bkumar48 <bittukumarinfo48@gmail.com>
2026-05-12 23:46:35 +0530

    Add linear_search method to arrays.java

--- arrays.java
+++ arrays.java
@@ -1,26 +1,23 @@
 import java.util.*;
 
 public class arrays {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int size = sc.nextInt();
-        
-        int[] arr = new int[size];
 
-        // input
-        for (int i = 0; i < size; i++) {
-            arr[i] = sc.nextInt();
-        }
-
-        // find max
-        int max = arr[0];
-        for (int i = 1; i < size; i++) {
-            if (arr[i] > max) {
-                max = arr[i];
+    public static int linear_search(int arr[], int key) {
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == key) {
+                return i;
             }
         }
+        return -1;
+    }
 
-        System.out.println(max);
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int numbers[] = { 2, 4, 6, 8, 10, 12, 14, 16, 18, 20 };
+        int key = 14;
+
+        System.out.println("Index: " + linear_search(numbers, key));
+
         sc.close();
     }
 }
